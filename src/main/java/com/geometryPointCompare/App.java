87443d68ed7b95f79c74804e9105db5fd12f7d9a
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 20:42:18 +0530

    Adding java project with welocme message

---
+++ src/main/java/com/geometryPointCompare/App.java
@@ -0,0 +1,15 @@
+package com.geometryPointCompare;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "---------------welcome----------------" );
+    }
+}
